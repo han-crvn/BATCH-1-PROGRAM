c7d8798433861660f7efb03f451aed4a24ece0d9
Shane <palomoshanecraven@gmail.com>
2025-03-06 00:11:43 +0800

    update

--- Prog 08.py
+++ Prog 08.py
@@ -5,7 +5,7 @@ nums = []
 for i in range(10):
     while True:
         try:
-            num = float(input(f"Enter number {i + 1}: "))
+            num = int(input(f"Enter number {i + 1}: "))
 
             if num % 2 != 0:
                 nums.append(num)
